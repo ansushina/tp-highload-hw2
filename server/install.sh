40b263860895c0f57e0164eb37807a5aecef06ba
ansushina <nastya.sushina@yandex.ru>
2020-04-14 11:13:42 +0300

    update install.sh

--- server/install.sh
+++ server/install.sh
@@ -1,16 +1,15 @@
-apt-get update
-apt-get install nginx
+apt-get update -y
+apt-get install nginx -y
 sudo nginx
 cp -f ./nginx.conf /etc/nginx/nginx.conf
 cp -f ./default /etc/nginx/sites-available/default
 sudo nginx -s reload
-apt update
-apt install apt-transport-https ca-certificates curl software-properties-common
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt update -y
+apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt update
+apt update -y
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install docker-ce -y
+./start.sh
 sudo systemctl status docker
-
